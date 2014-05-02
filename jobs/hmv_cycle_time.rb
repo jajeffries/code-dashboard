28d3a97a212709c1598fe500bdaff2305ff1c6dd
iainjmitchell <iainjmitchell@gmail.com>
2014-05-02 09:34:02 +0100

    adding dev cycle time

--- jobs/hmv_cycle_time.rb
+++ jobs/hmv_cycle_time.rb
@@ -2,18 +2,25 @@ require 'TrelloCycleTime'
 
 HMV_BOARD_ID = '5aJf2ZMz'
 ACCESS_TOKEN = ENV['TRELLO_TOKEN']
-puts ACCESS_TOKEN
+
+
 
 trello_cycle_time = AgileTrello::TrelloCycleTime.new(
 	public_key: 'e185a8128064891a8961802a3d86b08e',
 	access_token: ACCESS_TOKEN
 )
 
-SCHEDULER.every '60m', :first_in => 0 do
+SCHEDULER.every '12h', :first_in => 0 do
   	average_cycle_time = trello_cycle_time.get(
 		board_id: HMV_BOARD_ID,
 		start_list: 'In Progress',
 		end_list: 'Ready for Release'
 	)
   	send_event('hmv_cycle_time', { text: average_cycle_time })
+  	average_cycle_time_dev = trello_cycle_time.get(
+		board_id: HMV_BOARD_ID,
+		start_list: 'In Progress',
+		end_list: 'Ready for Test'
+	)
+  	send_event('hmv_cycle_time_dev', { text: average_cycle_time_dev })
 end
