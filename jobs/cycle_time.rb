5ba2901ac55aa10bcb49bfb061d5918e6fa6369e
iainjmitchell <iainjmitchell@gmail.com>
2014-07-21 10:08:22 +0100

    adding apache board

--- jobs/cycle_time.rb
+++ jobs/cycle_time.rb
@@ -14,6 +14,12 @@ app_cycle_times = BoardCycleTimeEventManager.new(APP_BOARD_ID, [
 	BoardCycleTimeEvent.new(event_name: 'app_cycle_time_dev', start_list: 'In Progress', end_list: 'Ready for Test')
 ])
 
+APACHE_BOARD_ID = 'rix53wyv'
+app_cycle_times = BoardCycleTimeEventManager.new(APACHE_BOARD_ID, [
+	BoardCycleTimeEvent.new(event_name: 'apache_cycle_time', start_list: 'In Progress', end_list: 'Ready for Release'),
+	BoardCycleTimeEvent.new(event_name: 'apache_cycle_time_dev', start_list: 'In Progress', end_list: 'Ready for Test')
+])
+
 SCHEDULER.every '12h', :first_in => 0 do
   	hmv_cycle_time.retrieve
   	app_cycle_times.retrieve
