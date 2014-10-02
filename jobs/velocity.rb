3ed32406479c08639fca19f37df9d96a66c77594
iainjmitchell <iainjmitchell@gmail.com>
2014-10-02 15:07:06 +0100

    adding velocity to app and hmv boards

--- jobs/velocity.rb
+++ jobs/velocity.rb
@@ -7,15 +7,17 @@ BOARD = [
 
 MONGO_CONN = ENV['MONGO_CONN']
 
-repository = MongoRepository.new(MONGO_CONN, 'production', 'velocity')
+if MONGO_CONN
+	repository = MongoRepository.new(MONGO_CONN, 'production', 'velocity')
 
-SCHEDULER.every '6h', :first_in => 0 do
-	BOARD.each do | board |
-		board_data = repository.get(board['_id'])
-		points = board_data['velocities'].map do | velocity_data |
-			date = DateTime.parse(velocity_data['date'])
-			{x: date.to_time.getutc.to_i, y: velocity_data['velocity']}
+	SCHEDULER.every '6h', :first_in => 0 do
+		BOARD.each do | board |
+			board_data = repository.get(board['_id'])
+			points = board_data['velocities'].map do | velocity_data |
+				date = DateTime.parse(velocity_data['date'])
+				{x: date.to_time.getutc.to_i, y: velocity_data['velocity']}
+			end
+			send_event(board['event'], {'velocities' => points})
 		end
-		send_event(board['event'], {'velocities' => points})
 	end
 end
