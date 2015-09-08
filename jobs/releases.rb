0e8f385ad9808f6e6c11852ac159a4d2f3f3857a
unnamed.code.dev <unnamed.code.dev@example.com>
2015-09-08 15:52:07 +0100

    Stop releases job calling for data

--- jobs/releases.rb
+++ jobs/releases.rb
@@ -1,21 +1,21 @@
-require_relative '../lib/MongoRepository'
+# require_relative '../lib/MongoRepository'
 
-BOARD = [
-	{'_id' => 'HMV', 'event' => 'HMV-releases'}
-]
+# BOARD = [
+# 	{'_id' => 'HMV', 'event' => 'HMV-releases'}
+# ]
 
-MONGO_CONN = ENV['MONGO_CONN']
+# MONGO_CONN = ENV['MONGO_CONN']
 
-if MONGO_CONN
+# if MONGO_CONN
 
-	repository = MongoRepository.new(MONGO_CONN, 'production', 'release_tracking')
+# 	repository = MongoRepository.new(MONGO_CONN, 'production', 'release_tracking')
 
-	SCHEDULER.every '15m', :first_in => 0 do
-		BOARD.each do | board |
+# 	SCHEDULER.every '15m', :first_in => 0 do
+# 		BOARD.each do | board |
 			
-			release_data = repository.get(board['_id'])
+# 			release_data = repository.get(board['_id'])
 
-			send_event(board['event'], {'releases' => release_data['Releases']})
-		end
-	end
-end
+# 			send_event(board['event'], {'releases' => release_data['Releases']})
+# 		end
+# 	end
+# end
